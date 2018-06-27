b889a4b45dd1c6ede56002a603f67b5f5d2f60d6
gerardo <gerardomalpartidavela@gmail.com>
2018-06-27 12:20:58 -0700

    vanilla split done

--- d01/ex03/ft_split.js
+++ d01/ex03/ft_split.js
@@ -1,7 +1,41 @@
 function	ft_split(str) {
-	// if typeof string of chars continue
-	// else do nothing
-	// alocate a variable this is gonna return a variable an array 
-	// have a buffer that collect character by characters until it find a space or tab
+	let		len;
+	let		string;
+	let		buffer;
+	let		i;
+	let		holder;
+
+	holder = [];
+	buffer = "";
+	string = str.toString();
+	len = string.length;
+	i = 0;
+	if (typeof str != 'string')
+		return (holder);
+	while (i < len) {
+		if (string[i] != '\n' && string[i] !=  '\t' && string[i] != ' ') {
+			while((string[i] != '\n' && string[i] !=  '\t' && string[i] != ' ') && i < len) {
+				buffer += string[i];
+				++i;
+			}
+			holder.push(buffer);
+			buffer = "";
+		}
+		else
+			;
+		++i;
+	}
+	return (holder);
 };
 
+
+const	i = -1;
+const	test1 = ft_split("esta pendejada deberia no fallar");
+const	test2 = ft_split("");
+const	test3 = ft_split("             ");
+const	test4 = ft_split(45);
+
+console.log(test1);
+console.log(test2);
+console.log(test3);
+console.log(test4);
