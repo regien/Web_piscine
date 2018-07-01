f4a697f9b0652fc1af6bcc3c4b2654447eb62ebe
regien <gerardomalpartidavela@gmail.com>
2018-07-01 04:32:05 -0700

    checkpoint rush

---
+++ d03/ex04/test.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+	if (req.method === "GET") {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.write("<html><body>Hello</body></html>\n");
+		res.end();
+	}
+
+}).listen(8100);
