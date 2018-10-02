528b897fd8abf550f892b4ef37bed38959c64f3a
regien <gerardomalpartidavela@gmail.com>
2018-10-01 23:29:19 -0700

    ex03 lighter

--- d01/ex03/ft_split.js
+++ d01/ex03/ft_split.js
@@ -1,41 +1,11 @@
-function	ft_split(str) {
-	let		len;
-	let		string;
-	let		buffer;
-	let		i;
-	let		holder;
-
-	holder = [];
-	buffer = "";
-	string = str.toString();
-	len = string.length;
-	i = 0;
-	if (typeof str != 'string')
-		return (holder);
-	while (i < len) {
-		if (string[i] != '\n' && string[i] !=  '\t' && string[i] != ' ') {
-			while((string[i] != '\n' && string[i] !=  '\t' && string[i] != ' ') && i < len) {
-				buffer += string[i];
-				++i;
-			}
-			holder.push(buffer);
-			buffer = "";
-		}
-		else
-			;
-		++i;
+const	ft_split = (str) => {
+	let	holder = [];
+	let buffer = str.trim().split(/\s+/);
+	for (let i = buffer.length - 1; i >= 0; --i) {
+		holder.push(buffer[i]);
 	}
 	return (holder);
-};
+}
 
-
-const	i = -1;
-const	test1 = ft_split("esta pendejada deberia no fallar");
-const	test2 = ft_split("");
-const	test3 = ft_split("             ");
-const	test4 = ft_split(45);
-
-console.log(test1);
-console.log(test2);
-console.log(test3);
-console.log(test4);
+console.log(ft_split("Hello     World  AAA"));
+console.log(ft_split("      Testing     ONE     O   ONE"));
